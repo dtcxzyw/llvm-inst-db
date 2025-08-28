3f40603adf310060a2e67a32ab46ef6b65422eb9
Yingwei Zheng <dtcxzyw2333@gmail.com>
2025-08-29 02:19:35 +0800

    feat: add support for most targets

--- process.py
+++ process.py
@@ -3,6 +3,49 @@
 
 import json
 import sys
+import subprocess
+import tqdm
+import os
+
+SUPPORTED_TARGETS = [
+    "RISCV",
+    "X86",
+    "AArch64",
+    "ARM",
+    "LoongArch",
+    "SystemZ",
+    "PowerPC",
+    "AMDGPU",
+    "NVPTX",
+    "WebAssembly",
+    "BPF",
+    "SPIRV",
+]
+PROPERTIES_LIST = [
+    "isReturn",
+    "isBranch",
+    "isEHScopeReturn",
+    "isIndirectBranch",
+    "isCompare",
+    "isMoveImm",
+    "isMoveReg",
+    "isBitcast",
+    "isSelect",
+    "isBarrier",
+    "isCall",
+    "isAdd",
+    "isTrap",
+    "mayLoad",
+    "mayStore",
+    "mayRaiseFPException",
+    "isTerminator",
+    "hasDelaySlot",
+    "hasCtrlDep",
+    "isNotDuplicable",
+    "isConvergent",
+    "isAsCheapAsAMove",
+    "hasSideEffects",
+]
 
 
 def convert_operand_list(args):
@@ -11,10 +54,12 @@ def convert_operand_list(args):
         res.append(f"{arg['printable']}:{identifier}")
     return res
 
+
 def encode_var_bit(var, msb, lsb):
     if msb == lsb:
-        return var + '{' + str(msb) + '}'
-    return var + '{' + str(msb) + '-' + str(lsb) + '}'
+        return var + "{" + str(msb) + "}"
+    return var + "{" + str(msb) + "-" + str(lsb) + "}"
+
 
 def encode_inst(encoding):
     bit_string = ""
@@ -22,17 +67,18 @@ def encode_inst(encoding):
     var_msb = None
     var_lsb = None
     for bit in reversed(encoding):
-        if isinstance(bit, int):
+        if bit is None:
+            bit_string += "?"
+        elif isinstance(bit, int):
             if var:
                 bit_string += encode_var_bit(var, var_msb, var_lsb)
                 var = None
             bit_string += str(bit)
-        elif bit['kind'] == 'var':
-            bit_string += bit['var']
-        else:
-            assert bit['kind'] == 'varbit'
-            new_var = bit['var']
-            new_idx = bit['index']
+        elif bit["kind"] == "var":
+            bit_string += bit["var"]
+        elif bit["kind"] == "varbit":
+            new_var = bit["var"]
+            new_idx = bit["index"]
             if var is None:
                 var = new_var
                 var_msb = var_lsb = new_idx
@@ -42,12 +88,17 @@ def encode_inst(encoding):
                 bit_string += encode_var_bit(var, var_msb, var_lsb)
                 var = new_var
                 var_msb = var_lsb = new_idx
+        else:
+            assert bit["kind"] == "complex"
+            return None
     return bit_string
 
-if __name__ == "__main__":
-    with open(sys.argv[1]) as f:
+
+def convert_json(input_json, output_json):
+    with open(input_json) as f:
         obj = json.load(f)
-    for item in obj.values():
+    output_list = []
+    for item in tqdm.tqdm(obj.values()):
         if not isinstance(item, dict):
             continue
         if item.get("!anonymous", True):
@@ -56,7 +107,8 @@ if __name__ == "__main__":
         if not name:
             continue
         fields = item.get("!fields", [])
-        if "Inst" in fields:
+        superclasses = item.get("!superclasses", [])
+        if "Inst" in fields or "Instruction" in superclasses:
             if item["isCodeGenOnly"] == 1:
                 continue
             if item["isPseudo"] == 1:
@@ -66,7 +118,6 @@ if __name__ == "__main__":
             # print(json.dumps(item, indent=2))
             inst_obj = dict()
             inst_obj["Name"] = name
-            inst_obj["Size"] = item["Size"]
             inst_obj["AsmString"] = item["AsmString"]
             if item["DecoderNamespace"] != "":
                 inst_obj["DecoderNamespace"] = item["DecoderNamespace"]
@@ -84,38 +135,55 @@ if __name__ == "__main__":
                 predicates.append(pred["printable"])
             if len(predicates) > 0:
                 inst_obj["Predicates"] = predicates
-            inst_obj["Encoding"] = encode_inst(item["Inst"])
+            if "Inst" in item:
+                inst_obj["Size"] = item["Size"]
+                inst_encoding = encode_inst(item["Inst"])
+                if inst_encoding:
+                    inst_obj["Encoding"] = inst_encoding
+            elif "X86Inst" in superclasses:
+                pass
+            elif "WebAssemblyInst" in superclasses:
+                pass
 
-            properties_list = [
-                "isReturn",
-                "isBranch",
-                "isEHScopeReturn",
-                "isIndirectBranch",
-                "isCompare",
-                "isMoveImm",
-                "isMoveReg",
-                "isBitcast",
-                "isSelect",
-                "isBarrier",
-                "isCall",
-                "isAdd",
-                "isTrap",
-                "mayLoad",
-                "mayStore",
-                "mayRaiseFPException",
-                "isTerminator",
-                "hasDelaySlot",
-                "hasCtrlDep",
-                "isNotDuplicable",
-                "isConvergent",
-                "isAsCheapAsAMove",
-                "hasSideEffects",
-            ]
             properties = []
-            for key in properties_list:
+            for key in PROPERTIES_LIST:
                 if item.get(key, False):
                     properties.append(key)
             if len(properties) > 0:
                 inst_obj["Properties"] = properties
-            print(json.dumps(inst_obj, indent=2))
-            # exit()
+            output_list.append(inst_obj)
+    with open(output_json, "w") as f:
+        json.dump(output_list, f)
+
+
+if __name__ == "__main__":
+    llvm_src = sys.argv[1]
+    llvm_tblgen = sys.argv[2]
+    build_dir = "build"
+    os.makedirs(build_dir, exist_ok=True)
+    artifact_dir = os.path.join(build_dir, "artifact")
+    os.makedirs(artifact_dir, exist_ok=True)
+    include_dir = os.path.join(llvm_src, "llvm/include")
+    for target in SUPPORTED_TARGETS:
+        print("Converting", target)
+        intermediate_json = os.path.join(build_dir, target + ".json")
+        target_dir = os.path.join(llvm_src, "llvm/lib/Target", target)
+        target_td = target + ".td"
+        if target == "PowerPC":
+            target_td = "PPC.td"
+        if not os.path.exists(intermediate_json):
+            subprocess.check_call(
+                [
+                    llvm_tblgen,
+                    "--dump-json",
+                    os.path.join(target_dir, target_td),
+                    "-o",
+                    intermediate_json,
+                    "-I",
+                    include_dir,
+                    "-I",
+                    target_dir,
+                ]
+            )
+        artifact_json = os.path.join(artifact_dir, target + ".json")
+        convert_json(intermediate_json, artifact_json)
